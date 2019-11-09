e48f37e66de5a6ef2df055540b575f01272ec439
Craig Hammell <cilphex@gmail.com>
2019-11-08 21:18:06 -0800

    var name

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-import data from './data.js'
+import rawData from './data.js'
 import ChartData from './chart-data.js'
 import { drawChart as drawBasicChart } from './nlb-basic-chart.js'
 import { drawChart as drawRegressionChart } from './nlb-regression-chart.js'
 import { drawChart as drawExtrapolationChart } from './nlb-extrapolation-chart.js'
 import fillDataPoints from './fill-data-points.js'
 
-const chartData = new ChartData(data)
+const chartData = new ChartData(rawData)
 
 drawBasicChart(chartData)
 drawRegressionChart(chartData)
