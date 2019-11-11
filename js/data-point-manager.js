64a2e39727af08b20f4be43cd0438ae1d2668a0c
Craig Hammell <cilphex@gmail.com>
2019-11-10 16:37:23 -0800

    fill-data-points.js -> data-point-manager.js

---
+++ js/data-point-manager.js
@@ -0,0 +1,71 @@
+import d3 from './external/d3.js'
+import moment from './external/moment.js'
+import { moneyFormat } from './util.js'
+
+class DataPointManager {
+  constructor(chartData) {
+    this.chartData = chartData
+  }
+
+  fillData() {
+    this.fillToday()
+    this.fill5Years()
+  }
+
+  fillToday() {
+    const { regressionData, standardDeviation } = this.chartData
+
+    const todayData = regressionData.find(i =>
+      moment(i.date).isSame(moment(), 'day')
+    )
+
+    const { regressionNlb } = todayData
+
+    const todayExpected = Math.round(Math.pow(10, regressionNlb))
+    const todayMin = Math.round(Math.pow(10, regressionNlb - standardDeviation))
+    const todayMax = Math.round(Math.pow(10, regressionNlb + standardDeviation))
+
+    const vals = {
+      '#today_expected': todayExpected,
+      '#today_max': todayMax,
+      '#today_min': todayMin
+    }
+
+    Object.keys(vals).forEach(key =>
+      document.querySelector(key).textContent = moneyFormat(vals[key])
+    )
+  }
+
+  fill5Years() {
+    const { regressionData } = this.chartData
+
+    const years = Array(5).fill(null)
+      .map((item, i) => moment().year() + i)
+      .map((year, i) =>
+        regressionData.find(dataItem =>
+          moment(dataItem.date).isSame(moment(`${year}-01-01`), 'day')
+        )
+      )
+
+    var table = d3.select('#year_data_points')
+
+    var rows = table
+      .selectAll('tr')
+      .data(years)
+      .enter()
+      .append('tr')
+
+    var cells = rows.selectAll('td')
+      .data(d => [
+        moment(d.date).year(),
+        moneyFormat(Math.round(Math.pow(10, d.regressionNlb)))
+      ])
+      .enter()
+      .append('td')
+      .text(d => d)
+  }
+}
+
+export default DataPointManager
+
+
--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ import ChartData from './chart-data.js'
 import BasicChart from './nlb-basic-chart.js'
 import RegressionChart from './nlb-regression-chart.js'
 import ExtrapolationChart from './nlb-extrapolation-chart.js'
-import fillDataPoints from './fill-data-points.js'
+import DataPointManager from './data-point-manager.js'
 
 const chartData = new ChartData(rawData)
 
 new BasicChart(chartData).drawChart()
 new RegressionChart(chartData).drawChart()
 new ExtrapolationChart(chartData).drawChart()
-fillDataPoints(chartData)
+new DataPointManager(chartData).fillData()
 
 // document.querySelector('#regression_chart_range')
 //   .addEventListener('input', (e) => {
