3bc6723151bd3321bbcab5e94c37f4eb5adfa31e
Craig Hammell <cilphex@gmail.com>
2020-01-06 09:51:41 -0800

    data

--- js/data.js
+++ js/data.js
@@ -5,6 +5,8 @@
 // range on the number of items present, rather than the difference
 // in min and max date in the items in this set
 const data = [
+  { date: "Jan 05, 2020", price: "7,372.5" },
+  { date: "Jan 04, 2020", price: "7,376.8" },
   { date: "Jan 03, 2020", price: "7,343.1" },
   { date: "Jan 02, 2020", price: "6,967.0" },
   { date: "Jan 01, 2020", price: "7,199.8" },
