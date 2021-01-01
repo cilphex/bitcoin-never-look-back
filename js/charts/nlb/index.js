c45b3abf1a50509b3d1be76125527c727a5a368a
Craig Hammell <cilphex@gmail.com>
2020-12-31 20:54:45 -0500

    load data from json file that is fetched async

--- js/charts/nlb/index.js
+++ js/charts/nlb/index.js
@@ -1,13 +1,18 @@
-import rawData from '/js/data.js'
 import ChartData from '/js/chart-data.js'
 import BasicChart from './basic-chart.js'
 import RegressionChart from './regression-chart.js'
 import ExtrapolationChart from './extrapolation-chart.js'
 import DataPointManager from './data-point-manager.js'
 
-const chartData = new ChartData(rawData)
+function gotData(rawData) {
+  const chartData = new ChartData(rawData)
 
-new BasicChart(chartData)
-new RegressionChart(chartData)
-new ExtrapolationChart(chartData)
-new DataPointManager(chartData)
+  new BasicChart(chartData)
+  new RegressionChart(chartData)
+  new ExtrapolationChart(chartData)
+  new DataPointManager(chartData)
+}
+
+fetch('/data.json')
+  .then(res => res.json())
+  .then(gotData)
