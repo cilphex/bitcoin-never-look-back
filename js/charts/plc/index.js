3080ccb0fd94b29094ec8195797245e5a1685fbb
Craig Hammell <cilphex@gmail.com>
2021-01-12 15:23:58 -0600

    use data.json instead of data.js on plc page

--- js/charts/plc/index.js
+++ js/charts/plc/index.js
@@ -1,11 +1,16 @@
-import rawData from '/js/data.js'
 import ChartData from '/js/chart-data.js'
 import BasicChart from './basic-chart.js'
 import ExtrapolationChart from './extrapolation-chart.js'
 import DataPointManager from './data-point-manager.js'
 
-const chartData = new ChartData(rawData)
+function gotData(rawData) {
+  const chartData = new ChartData(rawData)
 
-new BasicChart(chartData)
-new ExtrapolationChart(chartData)
-new DataPointManager(chartData)
+  new BasicChart(chartData)
+  new ExtrapolationChart(chartData)
+  new DataPointManager(chartData)
+}
+
+fetch('/data.json')
+  .then(res => res.json())
+  .then(gotData)
