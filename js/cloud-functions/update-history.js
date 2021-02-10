d7a4576eb4325afc6d7fe849d2f2c821d7404460
Craig Hammell <cilphex@gmail.com>
2021-02-09 22:28:16 -0600

    checkin

--- js/cloud-functions/update-history.js
+++ js/cloud-functions/update-history.js
@@ -1,12 +1,21 @@
-async function updateHistory() {
+async function getCurrentData() {
+
+}
+
+async function getExchangeStats() {
   const res = await fetch('https://api.pro.coinbase.com/products/BTC-USD/stats');
   const data = await res.json();
-
   console.log('got data', data);
 }
 
-// exports = {
-//   updateHistory,
-// };
+async function updateHistory() {
+  let data;
+  let file;
 
-window.updateHistory = updateHistory;
+  await getCurrentData();
+  await getExchangeStats();
+}
+
+export default {
+  updateHistory,
+};
